afeb70b468ee0b1219054ee4e9145fa457f690c1
Combrand EGS <egscombrand@gmail.com>
2026-02-11 08:09:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9262077557-bc9c9",
+  "appId": "1:80532457942:web:b370536157cf3450243c77",
+  "apiKey": "AIzaSyDqMdXWhOikeYNqJo9XTMvZ63Hmmgixsfk",
+  "authDomain": "studio-9262077557-bc9c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "80532457942"
+};
