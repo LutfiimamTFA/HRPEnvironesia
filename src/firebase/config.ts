1c941b9bfc1c0f1f0e3e04ef31db1ce1de5a9307
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2026-02-12 06:29:20 +0000

    kenapa ini

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9262077557-bc9c9",
   "appId": "1:80532457942:web:b370536157cf3450243c77",
-  "storageBucket": "studio-9262077557-bc9c9.firebasestorage.com",
+  "storageBucket": "studio-9262077557-bc9c9.firebasestorage.app",
   "apiKey": "AIzaSyDqMdXWhOikeYNqJo9XTMvZ63Hmmgixsfk",
   "authDomain": "studio-9262077557-bc9c9.firebaseapp.com",
   "measurementId": "",
