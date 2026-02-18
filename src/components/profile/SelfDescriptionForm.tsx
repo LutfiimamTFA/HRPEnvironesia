c45607a3467afc7c7daf3cdc5bcc17c844938df1
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2026-02-18 02:04:38 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    FormDescription i

--- src/components/profile/SelfDescriptionForm.tsx
+++ src/components/profile/SelfDescriptionForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from '@/components/ui/button';
 import { Loader2 } from 'lucide-react';
